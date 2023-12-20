dafd486e3e335f8e37b9ce580fb821cb33cf410a
Kshitij Karandikar <kshitij.karandikar@mendix.com>
2023-12-20 14:22:22 +0100

    First commit

---
+++ src/CustomVictoryLineChart.tsx
@@ -0,0 +1,76 @@
+import { ReactElement, createElement } from "react";
+import { StyleSheet, View, TextStyle, ViewStyle } from "react-native";
+import { Style } from "@mendix/pluggable-widgets-tools";
+// import { HelloWorld } from "./components/HelloWorld";
+import { CustomVictoryLineChartProps, SeriesListType } from "../typings/CustomVictoryLineChartProps";
+import { VictoryArea, VictoryChart, VictoryStack, VictoryTheme } from "victory-native";
+
+export interface CustomStyle extends Style {
+    container: ViewStyle;
+    label: TextStyle;
+}
+
+export function CustomVictoryLineChart({
+    // Get properties (widget properties)
+    // style,
+    seriesList,
+    isStacked
+}: CustomVictoryLineChartProps<CustomStyle>): ReactElement {
+    if (!seriesList) {
+        return <View />;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    const getData = (series: SeriesListType) => {
+        // Iterate over list of datapoints and generate the data array
+        const dataToReturn = series.data.items?.map(item => {
+            return {
+                x: series.xAttr.get(item).value,
+                y: series.yAttr.get(item).value
+            };
+        });
+        return dataToReturn;
+    };
+
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    const renderCharts = () => {
+        // Iterate over all the series list and generate an Area/Line (TBD) chart
+        const chartsToReturn = seriesList.map((series, index) => {
+            return (
+                <VictoryArea
+                    key={index}
+                    data={getData(series)}
+                    style={{
+                        data: { stroke: series.color.value },
+                        parent: { border: "1px solid #ccc" }
+                    }}
+                />
+            );
+        });
+        // If stacked, render all area/line series inside a Victory stack
+        if (isStacked) {
+            return <VictoryStack>{chartsToReturn}</VictoryStack>;
+        } else {
+            return chartsToReturn;
+        }
+    };
+
+    // renders the components. All the charts are rendered by a function renderCharts inside a VictoryChart component, which itself is inside a view(container).
+    return (
+        <View style={styles.container}>
+            <VictoryChart width={350} theme={VictoryTheme.material}>
+                {renderCharts()}
+            </VictoryChart>
+        </View>
+    );
+}
+
+// Base styling. To be moved out of this file for clean up
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#f5fcff"
+    }
+});
---
+++ typings/CustomVictoryLineChartProps.d.ts
@@ -0,0 +1,46 @@
+/**
+ * This file was generated from CustomVictoryLineChart.xml
+ * WARNING: All changes made to this file will be overwritten
+ * @author Mendix Widgets Framework Team
+ */
+import { CSSProperties } from "react";
+import { DynamicValue, ListValue, ListAttributeValue } from "mendix";
+import { Big } from "big.js";
+
+export type TypeEnum = "line" | "area";
+
+export interface SeriesListType {
+    data: ListValue;
+    xAttr: ListAttributeValue<string | Big>;
+    yAttr: ListAttributeValue<Big>;
+    type: TypeEnum;
+    color: DynamicValue<string>;
+}
+
+export interface SeriesListPreviewType {
+    data: {} | { caption: string } | { type: string } | null;
+    xAttr: string;
+    yAttr: string;
+    type: TypeEnum;
+    color: string;
+}
+
+export interface CustomVictoryLineChartProps<Style> {
+    name: string;
+    style: Style[];
+    seriesList: SeriesListType[];
+    isStacked: boolean;
+}
+
+export interface CustomVictoryLineChartPreviewProps {
+    /**
+     * @deprecated Deprecated since version 9.18.0. Please use class property instead.
+     */
+    className: string;
+    class: string;
+    style: string;
+    styleObject?: CSSProperties;
+    readOnly: boolean;
+    seriesList: SeriesListPreviewType[];
+    isStacked: boolean;
+}
